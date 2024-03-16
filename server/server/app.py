3cfcfc1f3fa961639bc3ffbb0ad23edeae91a032
tushar-badlani <120042609+tushar-badlani@users.noreply.github.com>
2024-03-16 14:54:08 +0530

    Update app.py

--- server/server/app.py
+++ server/server/app.py
@@ -1,16 +1,51 @@
 from transformers import pipeline
+from bs4 import BeautifulSoup as BS
+import requests as req
+from transformers import pipeline
+sentiment_task = pipeline("sentiment-analysis", model="cardiffnlp/twitter-roberta-base-sentiment-latest", tokenizer="cardiffnlp/twitter-roberta-base-sentiment-latest")
+print(sentiment_task("Covid cases are increasing fast!"))
+
 
 # Load the text-to-text generation pipeline
 text_to_text_pipeline = pipeline("text2text-generation", model="google/flan-t5-small", tokenizer="google/flan-t5-small")
 
 
 # Example text input
-input_text = "I will give you a sentence in natural language and you will identify which stock i am talking about and return its ticker symbol. The sentence is: 'I want a safe investment and my friend reccomended ths company callled tcs should i invest'."
+text = ("I will give you a sentence in natural language "
+        "and you will identify which stock i am talking about and return its ticker symbol "
+        ". The sentence is:' ")
 
+prompt = input("Enter the sentence: ")
 
+input_text = text + prompt + "'"
 
 # Generate text based on the input
-generated_text = text_to_text_pipeline(input_text, max_length=100, do_sample = True ,temperature=0.5, top_k=50, top_p=0.95, num_return_sequences=1)
+result = text_to_text_pipeline(input_text, max_length=100, do_sample = True ,temperature=0.5, top_k=50, top_p=0.95, num_return_sequences=1)
 
-# Print the generated text
-print(generated_text[0]['generated_text'])
+url = f"https://www.businesstoday.in/topic/{result[0]['generated_text']}"
+print(url)
+
+webpage = req.get(url)  # YOU CAN EVEN DIRECTLY PASTE THE URL IN THIS
+# HERE HTML PARSER IS ACTUALLY THE WHOLE HTML PAGE
+trav = BS(webpage.content, "html.parser")
+
+# TO GET THE TYPE OF CLASS
+# HERE 'a' STANDS FOR ANCHOR TAG IN WHICH NEWS IS STORED
+list =[]
+for link in trav.find_all('h2'):
+        list.append(link.string)
+
+list.remove(None)
+list.remove("COMPANIES")
+list.remove("INDICES ANALYSIS")
+list.remove("MUTUAL FUNDS")
+list.remove("NEWS")
+list.remove("AMCs")
+
+for i in list:
+    print(i)
+
+list.remove(None)
+
+for i in list:
+    print(sentiment_task(i))
